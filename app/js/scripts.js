3ff1d0d3154d12d2012080075ab21ecdfa6bddab
Vera <verkaevg@.gmail.com>
2020-12-08 18:08:01 +0400

    index add

---
+++ app/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+   	$('.banner-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="prev-left"></button>',
+        nextArrow: '<button class="prev-right"></button>',
+        dots: true
+    });
+
+});
