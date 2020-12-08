b1d04ab0578c86abf94fbe16583d8e67d5729325
Vera <verkaevg@.gmail.com>
2020-12-08 18:49:25 +0400

    index add part2

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -8,4 +8,50 @@ $(document).ready(function () {
         dots: true
     });
 
+    $('.act-products__list').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="prev-left__n"></button>',
+        nextArrow: '<button class="prev-right__n"></button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    //dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true,                
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true,
+                    prevArrow: false,
+                    nextArrow: false
+                }
+            }
+        ]
+    });
+
 });
